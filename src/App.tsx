5310464c5f2c295ba717782b6207c17c59659a6b
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-09-06 23:06:31 -0600

    initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CardComp from "./components/CardComp/CardComp";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <CardComp />
+      </div>
+    </ThemeProvider>
   );
 }
 

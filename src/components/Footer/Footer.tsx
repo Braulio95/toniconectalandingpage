e3de482633f1a555b9cc6d2c59d9a8486482d1c5
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-09-20 09:30:52 -0600

    adding tootltips to the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Linkedin } from "../Icons/LinkedinIcon/Linkedin";
 import { colorPalette } from "../../styles/partials/colors";
 import { GithubIcon } from "../Icons/GithubIcon/GithubIcon";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { SubtitleComp } from "../SubtitleComp/SubtitleComp";
 
 export const Footer = () => {
@@ -25,6 +25,10 @@ export const Footer = () => {
     setGithubHovered(false);
   };
 
+  const renderTooltip = (message: string) => (
+    <Tooltip id="button-tooltip">{message}</Tooltip>
+  );
+
   return (
     <footer className="bg-light text-center text-lg-start text-muted">
       <div className="mt-auto"></div>
@@ -40,34 +44,50 @@ export const Footer = () => {
           </Row>
           <Row style={{ height: "40%" }}>
             <Col xxl={12} sm={12} style={{ padding: "16px" }}>
-              <Linkedin
-                onMouseEnter={handleLinkedInMouseEnter}
-                onMouseLeave={handleLinkedInMouseLeave}
-                height="25px"
-                width="25px"
-                color={colorPalette.strongestGreen}
-                style={{
-                  marginInline: "4px",
-                  cursor: "pointer",
-                  transform: LinkedInHovered ? "scale(1.25)" : "scale(1)",
-                  transition: "transform 0.3s ease",
-                }}
-              />
+              <OverlayTrigger
+                placement="bottom"
+                delay={{ show: 200, hide: 200 }}
+                overlay={renderTooltip("See my LinkedIn profile")}
+              >
+                <a href="https://www.linkedin.com/in/braulioestradaalfonso/">
+                  <Linkedin
+                    onMouseEnter={handleLinkedInMouseEnter}
+                    onMouseLeave={handleLinkedInMouseLeave}
+                    height="25px"
+                    width="25px"
+                    color={colorPalette.strongestGreen}
+                    style={{
+                      marginInline: "4px",
+                      cursor: "pointer",
+                      transform: LinkedInHovered ? "scale(1.25)" : "scale(1)",
+                      transition: "transform 0.3s ease",
+                    }}
+                  />
+                </a>
+              </OverlayTrigger>
             </Col>
             <Col xxl={12} sm={12} style={{ padding: "16px" }}>
-              <GithubIcon
-                onMouseEnter={GithubMouseEnter}
-                onMouseLeave={GithubMouseLeave}
-                height="25px"
-                width="30px"
-                color={colorPalette.strongestGreen}
-                style={{
-                  marginInline: "4px",
-                  cursor: "pointer",
-                  transform: GithubHovered ? "scale(1.25)" : "scale(1)",
-                  transition: "transform 0.3s ease",
-                }}
-              />
+              <OverlayTrigger
+                placement="bottom"
+                delay={{ show: 200, hide: 200 }}
+                overlay={renderTooltip("See my GitHub profile")}
+              >
+                <a href="https://github.com/Braulio95">
+                  <GithubIcon
+                    onMouseEnter={GithubMouseEnter}
+                    onMouseLeave={GithubMouseLeave}
+                    height="25px"
+                    width="30px"
+                    color={colorPalette.strongestGreen}
+                    style={{
+                      marginInline: "4px",
+                      cursor: "pointer",
+                      transform: GithubHovered ? "scale(1.25)" : "scale(1)",
+                      transition: "transform 0.3s ease",
+                    }}
+                  />
+                </a>
+              </OverlayTrigger>
             </Col>
           </Row>
         </Container>

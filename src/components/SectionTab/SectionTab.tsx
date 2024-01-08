0b0560e50f23b1bbf15307c8d3bdf79db1bbe79f
Braulio95 <braulioestradaalfonso@hotmail.com>
2024-01-08 00:23:45 -0600

    adding buttons

---
+++ src/components/SectionTab/SectionTab.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SectionTab.css";
+
+export const SectionTab = () => {
+  return <button className="tab-howit">SectionTab</button>;
+};
--- src/modules/HowItWorks/HowItWorks.tsx
+++ src/modules/HowItWorks/HowItWorks.tsx
@@ -1,36 +1,40 @@
 import React from "react";
 import "./HowItWorks.css";
 import Logo from "../../assets/navIcon/logo.png";
+import { SectionTab } from "../../components/SectionTab/SectionTab";
 interface MobileProps {
-    isMobile: boolean;
+  isMobile: boolean;
 }
 
 export const HowItWorks = ({ isMobile }: MobileProps) => {
-    return (
-        <div id="how-it-works" className="how">
-            <div className={`wrapper ${isMobile ? "wrapper-mobile" : ""}`}>
-                <div className="title-panel">
-                    <div className="title-banner">
-                        <img src={Logo} alt="toniconecta-logo" />
-                    </div>
-                    <div className="title-container">
-                        <div className="title-text">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Deleniti consequuntur laudantium cum aliquid
-                            explicabo.
-                        </div>
-                    </div>
-                </div>
-                <div className="info-panel">
-                    <div className="info-container">
-                        <div className="info-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quos asperiores itaque, a rerum vitae non
-                            omnis fuga excepturi.
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div id="how-it-works" className="how">
+      <div className={`wrapper ${isMobile ? "wrapper-mobile" : ""}`}>
+        <div className="title-panel">
+          <div className="title-banner">
+            <img src={Logo} alt="toniconecta-logo" />
+          </div>
+          <div className="title-container">
+            <div className="title-text">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Deleniti consequuntur laudantium cum aliquid explicabo.
+              </p>
+              <SectionTab />
+              <SectionTab />
+              <SectionTab />
             </div>
+          </div>
         </div>
-    );
+        <div className="info-panel">
+          <div className="info-container">
+            <div className="info-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+              asperiores itaque, a rerum vitae non omnis fuga excepturi.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
